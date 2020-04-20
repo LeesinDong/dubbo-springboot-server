d91879079ed782ec0fd8a059c74382cdf720c50b
dataiyangu <32248827@qq.com>
2020-04-21 06:57:19 +0800

    springboot dubbo 降级、容错、负载、注册中心  v2

---
+++ api/src/main/java/com/leesin/ISayHelloService.java
@@ -0,0 +1,11 @@
+package com.leesin;
+
+/**
+ * @description:
+ * @author: Leesin Dong
+ * @date: Created in 2020/4/20 9:07
+ * @modified By:
+ */
+public interface ISayHelloService {
+    String sayHello();
+}
